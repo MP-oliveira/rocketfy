ffcf40ece80378cb4a9505894a50a1417ebd9e40
Mauricio Oliveira <mau_oliver@hotmail.com>
2022-03-07 09:26:32 -0300

    feat: adding header e cards to the project

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import { Container, Label } from './styles'
+
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label  color='#7159c1'/>
+      </header>
+      <p>
+        fazer migração completa do servidor
+      </p>
+      <img src='https://rockeseat-cdn.s3-sa-east-1.amazonaws.com/profile.png' alt=''/>
+    </Container>
+  )
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export default function Header() {
+  return (
+   <Container>
+     <h1>Rocketfy</h1>
+   </Container>
+  )
+}
+
